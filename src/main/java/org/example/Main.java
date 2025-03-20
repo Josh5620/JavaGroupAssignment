16f1fcefc54e39b90b3f2cca336db8b03e22cf44
Josh5620 <josh.ong2005@gmail.com>
2025-03-20 12:10:04 +0800

    dsafdsfsgrzfdvcx
    yrujmdtnhbsfrgdvascax

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        File file = new File('users.json');
+        Scanner scanner = new Scanner(reader);
+
+
+        String line;
+        while(scanner.hasNext()){
+            System.out.println(line);
+        }
+
+
+
     }
 }
